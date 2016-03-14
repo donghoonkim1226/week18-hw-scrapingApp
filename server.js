0863cd3781e4c4631f9d331212bcf158c932d359
dong hoon kim <donghoonkim1226@gmail.com>
2016-03-14 11:11:15 -0400

    was in the wrong folder, pushing up all files

---
+++ server.js
@@ -0,0 +1,64 @@
+var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var request = require('request');
+var logger = require('morgan');
+
+var PORT = process.env.PORT || 7000;
+
+var app = express();
+
+/*-------------------------------------------------
+  HANDLEBAR LAYOUTS
+-------------------------------------------------*/
+
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+/*-------------------------------------------------
+  ACCESS TO PUBLIC FOLDER
+-------------------------------------------------*/
+
+app.use(express.static("public"));
+
+/*-------------------------------------------------
+  DATABASE CONNECTION & CONFIG - MONGO DB
+-------------------------------------------------*/
+
+mongoose.connect('mongodb://localhost/scrapingApp');
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+var mongojs = require('mongojs');
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(err) {
+  console.log('Database Error:', err);
+});
+
+/*-------------------------------------------------
+  REQUIRING MODELS
+-------------------------------------------------*/
+
+var Note = require('./models/noteModel.js');
+var Article = require('./models/articleModel.js');
+
+app.listen(PORT, function(){
+  console.log("Server listening at " + PORT);
+});
